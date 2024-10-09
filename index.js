bb377629fd43353729618d7d01dcdd0d96f172c9
Collins Kathurima <collinskathu313@gmail.com>
2024-10-09 03:44:10 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", ()=>{
+    const changing_p = document.getElementById("text")
+    changing_p.textContent = "This is really cool!"
+  });
+  
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,4 @@
-require ( './helpers.js' );
+~require ( './helpers.js' );
 
 describe( "index.js", () => {
   it( 'should change the text on the page when DOM content has loaded', function () {
